f0714d6e81d3516a5e75451961ff17b94ee0a846
astuyve <astuyve@gmail.com>
2019-04-15 15:43:19 -0500

    Test passing, all objects pulled from aws event mocks

--- lib/events/api-gateway-event.ts
+++
@@ -1,9 +0,0 @@
-import merge from "deepmerge"
-import * as event from "./api-gateway-event-template.json"
-import { APIGatewayEvent } from "aws-lambda"
-
-export class ApiGatewayEvent {
-  public generateEvent(body: any): APIGatewayEvent {
-    return merge(event as unknown as APIGatewayEvent, body) as APIGatewayEvent
-  }
-}
--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,19 @@
-import { DynamoStreamEvent } from './events/dynamo-stream-event'
+import { merge, cloneDeep } from 'lodash'
+import { APIGatewayEvent, ScheduledEvent, S3Event, KinesisStreamEvent, DynamoDBStreamEvent, SQSEvent, SNSEvent } from "aws-lambda"
 
-export function generateEvent(eventType: string, body: any) {
-  switch(eventType) {
-    case "http":
-      return
-    case "dynamo-stream-event":
-      return new DynamoStreamEvent().generateEvent(body)
+const dictionary: any = {
+  'aws:sns': require('./events/aws/sns-template.json') as SNSEvent,
+  'aws:sqs': require('./events/aws/sqs-template.json') as SQSEvent,
+  'aws:apiGateway': require('./events/aws/api-gateway-event-template.json') as APIGatewayEvent,
+  'aws:scheduled': require('./events/aws/scheduled-template.json') as ScheduledEvent,
+  'aws:s3': require('./events/aws/s3-template.json') as S3Event,
+  'aws:kinesis': require('./events/aws/kinesis-template.json') as KinesisStreamEvent,
+  'aws:dynamo': require('./events/aws/dynamo-stream-event-template.json') as DynamoDBStreamEvent
+}
+
+export function generateEvent(eventType: string, body: any): any {
+  const event = dictionary[eventType]
+  if (event) {
+    return merge(cloneDeep(event), body)
   }
 }
