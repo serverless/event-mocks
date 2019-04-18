9d7394292e0780b17ab8cec471186d2949843407
Chris McLeod <chris.mcleod@serverless.com>
2019-04-18 15:19:47 -0500

    Add strict type inference to createEvent

--- lib/index.spec.ts
+++ lib/index.spec.ts
@@ -1,238 +1,213 @@
-import { expect } from 'chai'
-import createEvent from './index'
+import { expect } from "chai";
+import createEvent from "./index";
 
-describe ('creating a new SNS event', () => {
-  it('should return a valid event', () => {
-    const event = createEvent('aws:sns',
-      {
-        Records: [{
+describe("creating a new SNS event", () => {
+  it("should return a valid event", () => {
+    const event = createEvent("aws:sns", {
+      Records: [
+        {
           Sns: {
-            Message: 'trigger-email',
-          },
-        }],
-      },
-    ) as any
-    expect(event.Records[0].Sns.Message).to.equal('trigger-email')
-    expect(event.Records[0].Sns.Type).to.equal('Notification')
-  })
-})
+            Message: "trigger-email"
+          }
+        }
+      ]
+    } as any);
+    expect(event.Records[0].Sns.Message).to.equal("trigger-email");
+    expect(event.Records[0].Sns.Type).to.equal("Notification");
+  });
+});
 
-describe('createSqsEvent()', () => {
-  it('should return SQS mocked event', () => {
-    const event = createEvent(
-      'aws:sqs',
-      {
-        Records: [{
+describe("createSqsEvent()", () => {
+  it("should return SQS mocked event", () => {
+    const event = createEvent("aws:sqs", {
+      Records: [
+        {
           body: {
-            foo: 'bar',
-          },
-        }],
-      },
-    ) as any
+            foo: "bar"
+          }
+        }
+      ]
+    } as any);
 
-    expect(event.Records[0].body.foo).to.equal('bar')
-    expect(event.Records[0].eventSource).to.equal('aws:sqs')
-  })
-})
+    expect(event.Records[0].body.foo).to.equal("bar");
+    expect(event.Records[0].eventSource).to.equal("aws:sqs");
+  });
+});
 
-describe('createApigEvent()', () => {
-  it('should return APIG mocked event', () => {
-    const event = createEvent(
-      'aws:apiGateway',
-      {
-        body: {
-          first_name: 'Sam',
-          last_name: 'Smith',
-        },
-      },
-    ) as any
+describe("createApigEvent()", () => {
+  it("should return APIG mocked event", () => {
+    const event = createEvent("aws:apiGateway", {
+      body: {
+        first_name: "Sam",
+        last_name: "Smith"
+      }
+    } as any);
 
-    expect(event.body.first_name).to.equal('Sam')
-    expect(event.body.last_name).to.equal('Smith')
-    expect(event.httpMethod).to.equal('GET')
-  })
-})
+    expect(event.body.first_name).to.equal("Sam");
+    expect(event.body.last_name).to.equal("Smith");
+    expect(event.httpMethod).to.equal("GET");
+  });
+});
 
-describe('createS3Event()', () => {
-  it('should return S3 mocked event', () => {
-    const event = createEvent(
-      'aws:s3',
-      {
-        Records: [{
+describe("createS3Event()", () => {
+  it("should return S3 mocked event", () => {
+    const event = createEvent("aws:s3", {
+      Records: [
+        {
           s3: {
             bucket: {
-              name: 'my-bucket-name',
+              name: "my-bucket-name"
             },
             object: {
-              key: 'object-key',
-            },
-          },
-        }],
-      },
-    ) as any
+              key: "object-key"
+            }
+          }
+        }
+      ]
+    } as any);
 
-    expect(event.Records[0].s3.bucket.name).to.equal('my-bucket-name')
-    expect(event.Records[0].s3.object.key).to.equal('object-key')
-    expect(event.Records[0].eventName).to.equal('ObjectCreated:Put')
-  })
+    expect(event.Records[0].s3.bucket.name).to.equal("my-bucket-name");
+    expect(event.Records[0].s3.object.key).to.equal("object-key");
+    expect(event.Records[0].eventName).to.equal("ObjectCreated:Put");
+  });
 
-  it('should return S3 mocked event without side-effect', () => {
-    const event = createEvent(
-      'aws:s3',
-      {
-        Records: [{
+  it("should return S3 mocked event without side-effect", () => {
+    const event = createEvent("aws:s3", {
+      Records: [
+        {
           s3: {
             bucket: {
-              name: 'my-bucket-name',
+              name: "my-bucket-name"
             },
             object: {
-              key: 'object-key',
-            },
-          },
-        }],
-      },
-    ) as any
+              key: "object-key"
+            }
+          }
+        }
+      ]
+    } as any);
 
-    const event2 = createEvent(
-      'aws:s3',
-      {
-        Records: [{
+    const event2 = createEvent("aws:s3", {
+      Records: [
+        {
           s3: {
             bucket: {
-              name: 'my-bucket-name',
+              name: "my-bucket-name"
             },
             object: {
-              key: 'object-key-2',
-            },
-          },
-        }],
-      },
-    ) as any
+              key: "object-key-2"
+            }
+          }
+        }
+      ]
+    } as any);
 
-    expect(event.Records[0].s3.bucket.name).to.equal('my-bucket-name')
-    expect(event.Records[0].s3.object.key).to.equal('object-key')
-    expect(event2.Records[0].s3.object.key).to.equal('object-key-2')
-    expect(event.Records[0].eventName).to.equal('ObjectCreated:Put')
-  })
-})
+    expect(event.Records[0].s3.bucket.name).to.equal("my-bucket-name");
+    expect(event.Records[0].s3.object.key).to.equal("object-key");
+    expect(event2.Records[0].s3.object.key).to.equal("object-key-2");
+    expect(event.Records[0].eventName).to.equal("ObjectCreated:Put");
+  });
+});
 
-describe('createScheduledEvent()', () => {
-  it('should return Scheduled mocked event', () => {
-    const event = createEvent(
-      'aws:scheduled',
-      {
-        region: 'us-west-2',
-      },
-    ) as any
+describe("createScheduledEvent()", () => {
+  it("should return Scheduled mocked event", () => {
+    const event = createEvent("aws:scheduled", {
+      region: "us-west-2"
+    } as any);
 
-    expect(event.region).to.equal('us-west-2')
-    expect(event['detail-type']).to.equal('Scheduled Event')
-  })
-})
+    expect(event.region).to.equal("us-west-2");
+    expect(event["detail-type"]).to.equal("Scheduled Event");
+  });
+});
 
-describe('createKinesisEvent()', () => {
-  it('should return Kinesis mocked event', () => {
-    const event = createEvent(
-      'aws:kinesis',
-      {
-        Records: [{
+describe("createKinesisEvent()", () => {
+  it("should return Kinesis mocked event", () => {
+    const event = createEvent("aws:kinesis", {
+      Records: [
+        {
           kinesis: {
-            data: Buffer.from('kinesis test').toString('base64'),
-          },
-        }],
-      },
-    ) as any
+            data: Buffer.from("kinesis test").toString("base64")
+          }
+        }
+      ]
+    } as any);
 
-    expect(Buffer.from(event.Records[0].kinesis.data, 'base64').toString('ascii')).to.equal('kinesis test')
-  })
-})
+    expect(
+      Buffer.from(event.Records[0].kinesis.data, "base64").toString("ascii")
+    ).to.equal("kinesis test");
+  });
+});
 
-describe('createCloudWatchEvent()', () => {
-  it('should return a valid event', () => {
-    const event = createEvent(
-      'aws:cloudWatch',
-      {
-        'detail-type': 'Something has been deleted.',
-        'region': 'us-east-1',
-      },
-    ) as any
-    expect(event['detail-type']).to.equal('Something has been deleted.')
-    expect(event.region).to.equal('us-east-1')
-  })
-})
+describe("createCloudWatchEvent()", () => {
+  it("should return a valid event", () => {
+    const event = createEvent("aws:cloudWatch", {
+      "detail-type": "Something has been deleted.",
+      region: "us-east-1"
+    } as any);
+    expect(event["detail-type"]).to.equal("Something has been deleted.");
+    expect(event.region).to.equal("us-east-1");
+  });
+});
 
-describe('createCloudWatchLogEvent()', () => {
-  it('should return a valid event', () => {
-    const event = createEvent(
-      'aws:cloudWatchLog',
-      {
-        awslogs: {
-          data: 'Some gzipped, then base64 encoded data',
-        },
-      },
-    ) as any
-    expect(event.awslogs.data).to.equal('Some gzipped, then base64 encoded data')
-  })
-})
+describe("createCloudWatchLogEvent()", () => {
+  it("should return a valid event", () => {
+    const event = createEvent("aws:cloudWatchLog", {
+      awslogs: {
+        data: "Some gzipped, then base64 encoded data"
+      }
+    }) as any;
+    expect(event.awslogs.data).to.equal(
+      "Some gzipped, then base64 encoded data"
+    );
+  });
+});
 
-describe('createAlexaSkillEvent()', () => {
-  it('should return a valid event', () => {
-    const event = createEvent(
-      'aws:alexaSkill',
-      {
-        request: {
-          type: 'CanFulfillIntentRequest',
-        },
-        context: {
-          System: {
-            deviceId: 'myDevice',
-          },
-        },
+describe("createAlexaSkillEvent()", () => {
+  it("should return a valid event", () => {
+    const event = createEvent("aws:alexaSkill", {
+      request: {
+        type: "CanFulfillIntentRequest"
       },
-    ) as any
-    expect(event.request.type).to.equal('CanFulfillIntentRequest')
-    expect(event.context.System.deviceId).to.equal('myDevice')
-  })
-})
+      context: {
+        System: {
+          deviceId: "myDevice"
+        }
+      }
+    } as any);
+    expect(event.request.type).to.equal("CanFulfillIntentRequest");
+    expect(event.context.System.deviceId).to.equal("myDevice");
+  });
+});
 
-describe('createAlexaSmartHomeEvent()', () => {
-  it('should return a valid event', () => {
-    const event = createEvent(
-      'aws:alexaSmartHome',
-      {
-        payload: {
-          switchControlAction: 'TURN_OFF',
-        },
-      },
-    ) as any
-    expect(event.payload.switchControlAction).to.equal('TURN_OFF')
-  })
-})
+describe("createAlexaSmartHomeEvent()", () => {
+  it("should return a valid event", () => {
+    const event = createEvent("aws:alexaSmartHome", {
+      payload: {
+        switchControlAction: "TURN_OFF"
+      }
+    } as any);
+    expect(event.payload.switchControlAction).to.equal("TURN_OFF");
+  });
+});
 
-describe('createIotEvent()', () => {
-  it('should return a valid event', () => {
-    const event = createEvent(
-      'aws:iot',
-      {
-        this: {
-          can: {
-            be: 'anything I want',
-          },
-        },
-      },
-    ) as any
-    expect(event.this.can.be).to.equal('anything I want')
-  })
-})
+describe("createIotEvent()", () => {
+  it("should return a valid event", () => {
+    const event = createEvent("aws:iot", {
+      this: {
+        can: {
+          be: "anything I want"
+        }
+      }
+    } as any);
+    expect(event.this.can.be).to.equal("anything I want");
+  });
+});
 
-describe('createCognitoPoolEvent()', () => {
-  it('should return a valid event', () => {
-    const event = createEvent(
-      'aws:cognitoUserPool',
-      {
-        userName: 'notAJ',
-      },
-    ) as any
-    expect(event.userName).to.eql('notAJ')
-  })
-})
+describe("createCognitoPoolEvent()", () => {
+  it("should return a valid event", () => {
+    const event = createEvent("aws:cognitoUserPool", {
+      userName: "notAJ"
+    } as any);
+    expect(event.userName).to.eql("notAJ");
+  });
+});
--- lib/index.ts
+++ lib/index.ts
@@ -1,41 +1,58 @@
-import { merge, cloneDeep } from 'lodash'
-import { APIGatewayEvent, ScheduledEvent, S3Event, KinesisStreamEvent, DynamoDBStreamEvent, SQSEvent, SNSEvent, CloudWatchLogsEvent, CognitoUserPoolEvent } from 'aws-lambda'
-import { AlexaSmartHomeEvent, AlexaSkillEvent, CloudWatchEvent } from 'aws-lambda'
-import { GeneratedEvent } from './generatedEvent'
-import snsTemplate from './events/aws/sns-template.json'
-import sqsTemplate from './events/aws/sqs-template.json'
-import apiGatewayTemplate from './events/aws/api-gateway-event-template.json'
-import scheduledTemplate from './events/aws/scheduled-template.json'
-import s3Template from './events/aws/s3-template.json'
-import kinesisTemplate from './events/aws/kinesis-template.json'
-import dynamoTemplate from './events/aws/dynamo-stream-event-template.json'
-import cloudwatchLogEventTemplate from './events/aws/cloud-watch-log-event-template.json'
-import alexaSmartHomeEventTemplate from './events/aws/alexa-smart-home-event-template.json'
-import alexaSkillEventTemplate from './events/aws/alexa-skill-event-template.json'
-import cloudWatchEventTemplate from './events/aws/cloud-watch-event-template.json'
-import cognitoUserPoolEventTemplate from './events/aws/cognito-user-pool-event-template.json'
+import { merge, cloneDeep, Dictionary } from "lodash";
+import {
+  APIGatewayEvent,
+  ScheduledEvent,
+  S3Event,
+  KinesisStreamEvent,
+  DynamoDBStreamEvent,
+  SQSEvent,
+  SNSEvent,
+  CloudWatchLogsEvent,
+  CognitoUserPoolEvent
+} from "aws-lambda";
+import {
+  AlexaSmartHomeEvent,
+  AlexaSkillEvent,
+  CloudWatchEvent
+} from "aws-lambda";
+import { GeneratedEvent } from "./generatedEvent";
+import snsTemplate from "./events/aws/sns-template.json";
+import sqsTemplate from "./events/aws/sqs-template.json";
+import apiGatewayTemplate from "./events/aws/api-gateway-event-template.json";
+import scheduledTemplate from "./events/aws/scheduled-template.json";
+import s3Template from "./events/aws/s3-template.json";
+import kinesisTemplate from "./events/aws/kinesis-template.json";
+import dynamoTemplate from "./events/aws/dynamo-stream-event-template.json";
+import cloudwatchLogEventTemplate from "./events/aws/cloud-watch-log-event-template.json";
+import alexaSmartHomeEventTemplate from "./events/aws/alexa-smart-home-event-template.json";
+import alexaSkillEventTemplate from "./events/aws/alexa-skill-event-template.json";
+import cloudWatchEventTemplate from "./events/aws/cloud-watch-event-template.json";
+import cognitoUserPoolEventTemplate from "./events/aws/cognito-user-pool-event-template.json";
 
-const dictionary: any = {
-  'aws:sns':  snsTemplate as SNSEvent,
-  'aws:sqs': sqsTemplate as SQSEvent,
-  'aws:apiGateway': apiGatewayTemplate as APIGatewayEvent,
-  'aws:scheduled': scheduledTemplate as ScheduledEvent,
-  'aws:s3': s3Template as S3Event,
-  'aws:kinesis': kinesisTemplate as KinesisStreamEvent,
-  'aws:dynamo': dynamoTemplate as DynamoDBStreamEvent,
-  'aws:cloudWatchLog': cloudwatchLogEventTemplate as CloudWatchLogsEvent,
-  'aws:alexaSmartHome': alexaSmartHomeEventTemplate as AlexaSmartHomeEvent,
-  'aws:alexaSkill': alexaSkillEventTemplate as AlexaSkillEvent,
-  'aws:cloudWatch': cloudWatchEventTemplate as CloudWatchEvent,
-  'aws:iot': {},
-  'aws:cognitoUserPool': cognitoUserPoolEventTemplate as CognitoUserPoolEvent,
-}
+const dictionary = {
+  "aws:sns": snsTemplate as SNSEvent,
+  "aws:sqs": sqsTemplate as SQSEvent,
+  "aws:apiGateway": apiGatewayTemplate as APIGatewayEvent,
+  "aws:scheduled": scheduledTemplate as ScheduledEvent,
+  "aws:s3": s3Template as S3Event,
+  "aws:kinesis": kinesisTemplate as KinesisStreamEvent,
+  "aws:dynamo": dynamoTemplate as DynamoDBStreamEvent,
+  "aws:cloudWatchLog": cloudwatchLogEventTemplate as CloudWatchLogsEvent,
+  "aws:alexaSmartHome": alexaSmartHomeEventTemplate as AlexaSmartHomeEvent,
+  "aws:alexaSkill": alexaSkillEventTemplate as AlexaSkillEvent,
+  "aws:cloudWatch": cloudWatchEventTemplate as CloudWatchEvent,
+  "aws:iot": {},
+  "aws:cognitoUserPool": cognitoUserPoolEventTemplate as CognitoUserPoolEvent
+};
 
-export default function createEvent(eventType: string, body: any): GeneratedEvent {
-  const event = dictionary[eventType]
-  let generatedEvent = {} as GeneratedEvent
+export default function createEvent<T extends keyof typeof dictionary, B>(
+  eventType: T,
+  body: typeof dictionary[T]
+): typeof dictionary[T] {
+  const event = dictionary[eventType];
+  let generatedEvent = {};
   if (event) {
-    generatedEvent = merge(cloneDeep(event), body)
+    generatedEvent = merge(cloneDeep(event), body);
   }
-  return generatedEvent
+  return generatedEvent;
 }
